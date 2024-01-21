918d7427fac933ed13037ff47d18e1e494feb314
MadhuriTeli <madhuriteli07@gmail.com>
2024-01-21 16:00:06 +0530

    Dropdown

---
+++ src/components/Dropdown.js
@@ -0,0 +1,53 @@
+import { useEffect, useRef, useState } from 'react';
+import { GoChevronDown } from 'react-icons/go';
+import Panel from './Panel';
+
+function Dropdown({ options , value, OnChange}) {
+    const [isOpen, setIsOpen] = useState(false);
+    const divEl = useRef();
+
+
+
+    useEffect(() => {
+        const handler = (event) => {
+            if (!divEl.current) {
+                return;
+            }
+
+            if (!divEl.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+            console.log(divEl.current);
+        }
+
+        document.addEventListener('click', handler, true);
+        return () => {
+            document.removeEventListener('click', handler);
+        };
+    }, []);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+    
+    const handleOptionClick = (option) => {
+        setIsOpen(false);
+        OnChange(option)
+    }
+
+  const renderedOptions = options.map((option) => {
+    return <div className='hover:bg-sky-100 rounded cursor-pointer P-1' onClick={()=>handleOptionClick(option)} key={option.value}>{option.label}</div>;
+  });
+    
+
+  return (
+    <div ref={divEl} className='w-48 relative'>
+          <Panel className='flex justify-between items-Center cursor-pointer' onClick={handleClick}>{value?.label || 'Select ...'}
+              <GoChevronDown className='text-lg' />
+          </Panel>
+      {isOpen && <Panel className='absolute top-full'>{renderedOptions}</Panel>}
+    </div>
+  );
+}
+
+export default Dropdown;
